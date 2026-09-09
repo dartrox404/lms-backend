55e7ac0e71dba5ba3dfa89d4d59fa0ad97093743
dartrox404 <arslanjaved57420@gmail.com>
2026-09-09 22:25:16 +0500

    feat: implement Pagination/filter/search

--- src/modules/student/student_repo.js
+++ src/modules/student/student_repo.js
@@ -1,7 +1,63 @@
 // modules/student/student.repository.js
+const student_model = require("./student_model");
 const StudentModel = require("./student_model");
 
-const findAll = () => StudentModel.find().sort({ createdAt: -1 });
+// const findAll = () => StudentModel.find().sort({ createdAt: -1 });
+const findAll = async ({
+  page,
+  limit,
+  department,
+  semester,
+  minCgpa,
+  maxCgpa,
+  search,
+  sortBy,
+  sortOrder,
+}) => {
+  const filter = {};
+
+  if (department) {
+    filter.department = department;
+  }
+  if (semester) {
+    filter.semester = semester;
+  }
+
+  if (minCgpa !== undefined || maxCgpa !== undefined) {
+    filter.cgpa = {};
+    if (minCgpa !== undefined) {
+      filter.cgpa.$gte = minCgpa;
+    }
+    if (maxCgpa !== undefined) {
+      filter.cgpa.$lte = maxCgpa;
+    }
+  }
+
+  if (search) {
+    const safeSearch = search.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    filter.$or = [
+      { name: { $regex: safeSearch, $options: "i" } },
+      { rollNumber: { $regex: safeSearch, $options: "i" } },
+    ];
+  }
+
+  const skip = (page - 1) * limit;
+
+  const sort = {
+    [sortBy]: sortOrder === "asc" ? 1 : -1,
+    _id: 1,
+  };
+
+  const [students, total] = await Promise.all([
+    StudentModel.find(filter).sort(sort).skip(skip).limt(limit).lean(),
+    StudentModel.countDocuments(filter),
+  ]);
+  return {
+    students,
+    total,
+  };
+};
+
 const findById = (id) => StudentModel.findById(id);
 const create = (data) => StudentModel.create(data);
 const updateById = (id, data) =>
--- src/validator/student_validator.js
+++ src/validator/student_validator.js
@@ -75,7 +75,70 @@ const updateValidator = Joi.object({
   }),
 }).min(1); // at least one field to update
 
+const getStudentsQueryValidator = Joi.object({
+  page: Joi.number().integer().min(1).default(1).messages({
+    "number.base": "Page must be a number.",
+    "number.integer": "Page must be an integer.",
+    "number.min": "Page must be at least 1.",
+  }),
+
+  limit: Joi.number().integer().min(1).max(100).default(10).messages({
+    "number.base": "Limit must be a number.",
+    "number.integer": "Limit must be an integer.",
+    "number.min": "Limit must be at least 1.",
+    "number.max": "Limit cannot exceed 100.",
+  }),
+
+  department: Joi.string()
+    .valid("CS", "SE", "IT", "EE", "ME", "CE")
+    .uppercase()
+    .messages({
+      "string.base": "Department must be a string.",
+      "any.only": "Department must be one of: CS, SE, IT, EE, ME, CE.",
+    }),
+
+  semester: Joi.number().integer().min(1).max(8).messages({
+    "number.base": "Semester must be a number.",
+    "number.integer": "Semester must be an integer.",
+    "number.min": "Semester must be at least 1.",
+    "number.max": "Semester cannot exceed 8.",
+  }),
+
+  minCgpa: Joi.number().min(0).max(4).messages({
+    "number.base": "minCgpa must be a number.",
+    "number.min": "minCgpa cannot be less than 0.",
+    "number.max": "minCgpa cannot exceed 4.",
+  }),
+
+  maxCgpa: Joi.number().min(0).max(4).messages({
+    "number.base": "maxCgpa must be a number.",
+    "number.min": "maxCgpa cannot be less than 0.",
+    "number.max": "maxCgpa cannot exceed 4.",
+  }),
+
+  search: Joi.string().trim().min(1).max(50).messages({
+    "string.base": "Search must be a string.",
+    "string.empty": "Search cannot be empty.",
+    "string.max": "Search cannot exceed 50 characters.",
+  }),
+
+  sortBy: Joi.string()
+    .valid("createdAt", "name", "rollNumber", "department", "semester", "cgpa")
+    .default("createdAt")
+    .messages({
+      "string.base": "sortBy must be a string.",
+      "any.only":
+        "sortBy must be one of: createdAt, name, rollNumber, department, semester, cgpa.",
+    }),
+
+  sortOrder: Joi.string().valid("asc", "desc").default("desc").messages({
+    "string.base": "sortOrder must be a string.",
+    "any.only": "sortOrder must be either asc or desc.",
+  }),
+});
+
 module.exports = {
   createValidator,
   updateValidator,
+  getStudentsQueryValidator,
 };
